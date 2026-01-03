76ac7d645d45fd3cab7e3ee48dea780125e1df4f
jawad-hakimee <jawadhakimee1401@gmail.com>
2026-01-04 00:51:53 +0430

    Connect frontend to deployed backend

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+export const API = axios.create({ baseURL: 'https://kabul-restaurant-app.onrender.com/' });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('userInfo')) {
